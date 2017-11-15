86564e06868d39f13c49667bec85c3b494e149ca
Lyricia <Lyricia01@gmail.com>
2017-11-15 20:02:59 +0900

    update
    update server, docs

--- Main Server/Main Server/MainServer.cpp
+++ Main Server/Main Server/MainServer.cpp
@@ -21,42 +21,81 @@ int recvn(SOCKET s, char *buf, int len, int flags)
 	return (len - left);
 }
 
-DWORD WINAPI testfunc(LPVOID arg) 
+DWORD WINAPI RecvMessage(LPVOID arg) 
 {
-	clientinfo* client = (clientinfo*)arg;
+	ConnectionInfo* client = (ConnectionInfo*)arg;
 	int retval;
 	char buf[64 + 1];
 	int len;
-	
+		
 	printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
-		inet_ntoa(client->clientaddr.sin_addr), ntohs(client->clientaddr.sin_port));
-
+		inet_ntoa(client->addr.sin_addr), ntohs(client->addr.sin_port));
+	
 	while (1) {
 		// 데이터 받기(고정 길이)
-		retval = recvn(client->clientsock, (char *)&len, sizeof(int), 0);
+		retval = recvn(client->sock, (char *)&len, sizeof(int), 0);
 		if (retval == SOCKET_ERROR) {
 			break;
 		}
 		else if (retval == 0)
 			break;
-
+	
 		// 데이터 받기(가변 길이)
-		retval = recvn(client->clientsock, buf, len, 0);
+		retval = recvn(client->sock, buf, len, 0);
 		if (retval == SOCKET_ERROR) {
 			break;
 		}
 		else if (retval == 0)
 			break;	
-
+	z
 		// 받은 데이터 출력
 		buf[retval] = '\0';
-		printf("[TCP/%s:%d] %s\n", inet_ntoa(client->clientaddr.sin_addr),
-			ntohs(client->clientaddr.sin_port), buf);
+		printf("[TCP/%s:%d] %s\n", inet_ntoa(client->addr.sin_addr),
+			ntohs(client->addr.sin_port), buf);
+
+		retval = send(client->sock, "test", sizeof("test"), NULL);
+		if (retval == SOCKET_ERROR)
+		{
+			closesocket(client->sock);
+			return 0;
+		}
+	}
+	
+	closesocket(client->sock);
+	printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n",
+		inet_ntoa(client->addr.sin_addr), ntohs(client->addr.sin_port));
+	return 0;
+}
+
+DWORD WINAPI RoomProcess(LPVOID arg) 
+{
+	RoomInfo* room = (RoomInfo*)arg;
+	HANDLE ClientMsgRecvThread[3];
+	int i = 0;
+	int retval;
+
+	for (auto c : room->clientlist)
+	{
+		ClientMsgRecvThread[c->ID] = CreateThread(NULL, 0, RecvMessage, (LPVOID)c, 0, NULL);
+		i++;
 	}
 
-	closesocket(client->clientsock);
-	printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n",
-		inet_ntoa(client->clientaddr.sin_addr), ntohs(client->clientaddr.sin_port));
+	while (1) {
+		room->clientlist.remove_if([&](ConnectionInfo* client)->bool{
+			DWORD exitcode = 0;
+			GetExitCodeThread(ClientMsgRecvThread[client->ID], &exitcode);
+			if(exitcode != STILL_ACTIVE)
+				cout << "Client ID : " << client->ID << " Terminated" << endl;
+			return (exitcode != STILL_ACTIVE);
+		});
+
+		if (room->clientlist.empty()) 
+		{
+			cout << "Terminate Room " << room->RoomID << endl;
+			ExitThread(NULL);
+		}
+		//cout << "Room " << room->RoomID << " is running client : " << room->clientlist.size() << endl;
+	}
 	return 0;
 }
 
@@ -66,29 +105,47 @@ void MainServer::Run()
 	SOCKADDR_IN clientaddr;
 	int addrlen;
 	int retval;
+	int roomcounter = 0;
+	int clientcounter = 0;
 	HANDLE hThread;
 
-	while (1) 
+	while (1)
 	{
 		addrlen = sizeof(clientaddr);
 		client_sock = accept(m_ListenSock, (SOCKADDR *)&clientaddr, &addrlen);
-		if (client_sock == INVALID_SOCKET) 
+		if (client_sock == INVALID_SOCKET)
 		{
 			cout << "client socket error" << endl;
 			break;
 		}
 
-		clientinfo* newclient = new clientinfo();
-		newclient->clientaddr = clientaddr;
-		newclient->clientsock = client_sock;
+		ConnectionInfo* newclient = new ConnectionInfo();
+		newclient->ID = clientcounter++;
+		newclient->addr = clientaddr;
+		newclient->sock = client_sock;
 		m_WaitingClientList.push_back(newclient);
-		hThread = CreateThread(NULL, 0, testfunc, (LPVOID)newclient, 0, NULL);
-		
-		if (hThread == NULL) 
+
+		if (m_WaitingClientList.size() >= 3)
 		{
-			cout << "Thread Creation failed" << endl;
-			//Assert
-			return;
+			RoomInfo* newroom = new RoomInfo();
+			newroom->RoomID = roomcounter++;
+
+			for (int i = 0; i < 3; ++i) 
+			{
+				newroom->clientlist.push_back(m_WaitingClientList.front());
+				m_WaitingClientList.pop_front();
+			}
+
+			hThread = CreateThread(NULL, 0, RoomProcess, (LPVOID)newroom, 0, NULL);
+			if (hThread == NULL) 
+			{
+				cout << "Thread Creation failed" << endl;
+				//Assert
+				return;
+			}
+
+			m_RoomList.push_back(newroom);
+			cout << "Create New Room " << newroom->RoomID << endl;
 		}
 	} 
 }
--- Main Server/Main Server/MainServer.h
+++ Main Server/Main Server/MainServer.h
@@ -1,19 +1,25 @@
 #pragma once
 
-class clientinfo {
-public:
-	SOCKET clientsock;
-	SOCKADDR_IN clientaddr;
+struct ConnectionInfo {
+	int				ID;
+	SOCKET			sock;
+	SOCKADDR_IN		addr;
 };
 
+struct RoomInfo {
+	int						RoomID;
+	list<ConnectionInfo*>	clientlist;
+	ConnectionInfo*			serverinfo;
+	list<NGPMSG*>			MsgQueue;
+};
 
 class MainServer : public Server
 {
 	//list<RoomInfo*>			m_RoomList;
 	//list<ClientInfo*>			m_WaitingClientList;
 	//ConnectedServerInfo		m_AgentServer;
-
-	list<clientinfo*>			m_WaitingClientList;
+	list<RoomInfo*>				m_RoomList;
+	list<ConnectionInfo*>		m_WaitingClientList;
 	list<NGPMSG*>				m_MsgQueue;
 
 public:
